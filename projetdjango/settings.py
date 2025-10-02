cea6b5ddcf46cadd062b497a0cb9f053dcc9a9dd
Achiraf balogun <adebayorbalogun65@gmail.com>
2025-10-02 17:03:40 +0200

    Premier commit

--- projetdjango/settings.py
+++ projetdjango/settings.py
@@ -77,15 +77,8 @@ WSGI_APPLICATION = 'projetdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bd django',
-        'USER': 'root',
-        'PASSWORD': 'monmotdepasse',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
